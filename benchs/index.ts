0241d828e83e322bd2015cee81fea3ad5dcdc1f7
Kane Wang <kane@yeecord.com>
2025-08-31 12:59:04 +0800

    chore: add benchmark script

---
+++ benchs/index.ts
@@ -0,0 +1,52 @@
+import { bench, do_not_optimize, run } from "mitata";
+import { codeToImageCore, loadFont } from "../dist/core.mjs";
+import { createHighlighter } from "shiki";
+import { Renderer } from "@takumi-rs/core";
+
+const exampleCode = /* js */ `
+import { writeFile } from "node:fs/promises";
+import { codeToImage } from "shiki-image";
+
+const buffer = await codeToImage('console.log("hello, world!");', {
+  lang: "js",
+  theme: "github-dark",
+  format: 'webp',
+  style: { borderRadius: 4 },
+});
+
+await writeFile("image.webp", buffer);
+`;
+
+const highlighter = await createHighlighter({
+  themes: ["github-dark"],
+  langs: ["js"],
+});
+
+// preload the font
+const renderer = new Renderer({
+  fonts: [await loadFont(undefined)],
+});
+
+bench("Takumi with default options", async () => {
+  // to make the benchmark more fair, 
+  // we purge the font rasterization/shaping cache before every run
+  renderer.purgeFontCache();
+
+  do_not_optimize(
+    await codeToImageCore(
+      exampleCode,
+      {
+        lang: "js",
+        theme: "github-dark",
+        format: "webp",
+        style: { borderRadius: 4 },
+      },
+      {
+        highlighter,
+        renderer,
+      },
+    ),
+  );
+});
+
+await run();
